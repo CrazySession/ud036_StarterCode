a11f204a542537021279f1a0ab0b14bdbbd819d7
MrA <coding@highyieldgaming.com>
2018-03-01 16:59:46 +0100

    changed formatting

--- media.py
+++ media.py
@@ -1,13 +1,16 @@
 from video import Video
 
+
 class Movies(Video):
     '''Main class as template to create new movies'''
     if __name__ == "__main__":
         print("This shouldn´t be run as main program "
               "please use the entertainment_center.py file")
 
-    def __init__(self, title, durration, storyline, poster_image_url, trailer_youtube_url):
-        Video.__init__(self,title,durration)
+    def __init__(
+            self, title, durration, storyline,
+            poster_image_url, trailer_youtube_url):
+        Video.__init__(self, title, durration)
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
